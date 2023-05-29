0c99b41449a39ccf6a7f32296059f2b00a1639ce
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2023-05-29 16:09:35 +0100

    trivial tweaks to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-extern crate core;
-use core::ffi::c_int;
+// extern crate core;
+// use core::ffi::c_int;
+
+use std::ffi::c_int;
 
 extern "C" {
     fn multiply(a: c_int, b: c_int) -> c_int;
@@ -7,12 +9,10 @@ extern "C" {
 
 fn main() {
     println!("[Rust] Hello from Rust! 🦀");
+    println!("[Rust] Calling function in C..");
 
     unsafe {
-        println!("[Rust] Calling function in C..");
-
         let result = multiply(5000, 5);
-
         println!("[Rust] Result: {}", result);
     }
 }
